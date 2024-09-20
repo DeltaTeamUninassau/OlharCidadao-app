a17dc2513cdd36b8de727278336b4e9c10e6bda3
Moacir Filho <100448770+MciFilho@users.noreply.github.com>
2024-09-19 22:50:34 -0300

    feat: ensuring the base structure of the app's screens - photo description, photo, send_list and result

---
+++ lib/photo_description.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class PhotoDescription extends StatelessWidget {
+  const PhotoDescription({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Olhar Cidadão'),
+      ),
+    );
+  }
+}
